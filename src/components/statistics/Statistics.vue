b5d51ec12ab43c702b12a6adb62a898d0367d318
Anqi <1518014096@qq.com>
2021-04-08 23:45:45 +0800

    Add new page: Statistics.vue

--- src/components/statistics/Statistics.vue
+++ src/components/statistics/Statistics.vue
@@ -1,10 +1,36 @@
 <template>
-  <div></div>
+  <div>
+    <el-breadcrumb>
+      <el-breadcrumb-item :to="{path:'/home'}">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>数据统计</el-breadcrumb-item>
+      <el-breadcrumb-item>数据报表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card>
+      <div id="pic" style="height: 300px"></div>
+    </el-card>
+  </div>
 </template>
 
 <script>
+import * as  echarts from 'echarts'
+
 export default {
-  name: "Statistics"
+  name: "Statistics",
+  data() {
+    return {
+      options: {}
+    }
+  },
+  methods: {
+    async initPic() {
+      let area = echarts.init(document.getElementById('pic'))
+      const {data} = await this.$http.get(`reports/type/1`)
+      area.setOption(data.data)
+    }
+  },
+  mounted() {
+    this.initPic()
+  }
 }
 </script>
 
